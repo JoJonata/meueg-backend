46a4df8e90414000a0a547cd2059cf9d88e5bb1c
Jonathan Araújo Rodrigues dos Santos <100201123+JoJonata@users.noreply.github.com>
2025-05-20 14:13:26 -0300

    Projeto inicial com Spring Boot e estrutura base

---
+++ src/main/java/br/ueg/meueg/MeuegBackendApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.meueg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuegBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuegBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ueg/meueg/MeuegBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.meueg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuegBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
